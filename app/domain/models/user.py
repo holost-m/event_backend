1b6a101ab433e059eab5db65f364139e93346bc5
liht <ispanovoleg@yandex.ru>
2025-11-29 21:09:54 +0300

    Написаны модели, добавлен requirements.txt

---
+++ app/domain/models/user.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+
+class User(BaseModel):
+    id_user: int
+    created_at: datetime
+    tg_username: str = Field(max_length=64)
+    full_name: str = Field(max_length=64)
+    last_seen: datetime
+    id_role: UUID
+
+
+class Permission(BaseModel):
+    title: str = Field(max_length=32)
+    description: str = Field(max_length=128)
+
+
+class Role(BaseModel):
+    id_role: UUID
+    name: str = Field(max_length=32)
+    permissions: list[str]
